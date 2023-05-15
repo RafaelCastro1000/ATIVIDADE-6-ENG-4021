c803af61a88bdd6c5ae3d7f24e4983f266ed5df6
Rafael <biduiochi@gmail.com>
2023-05-14 23:07:07 -0300

    Enviando primeira versão arquivos

---
+++ django_project/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path 
+from appRafael import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home,name="home"),
+    path('formtrofeus/', views.create_trofeus),
+    path('formjogadores/', views.create_jogadores),
+    path('trofeu/edit/<trofeu_id>', views.update_trofeu),
+    path('trofeu/delete/<trofeu_id>', views.delete_trofeu),
+    path('jogador/edit/<jogador_id>', views.update_jogador),
+
+]
+
