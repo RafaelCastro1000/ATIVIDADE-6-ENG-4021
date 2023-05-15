1b23646a013f7cb5c7984ee83555208b43f48f84
RafaelCastro1000 <129342660+RafaelCastro1000@users.noreply.github.com>
2023-05-14 23:33:39 -0300

    Update admin.py

--- appRafael/admin.py
+++ appRafael/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from .models import Trofeus,Jogadores
+from django.contrib import admin   #Importa o módulo admin
+from .models import Trofeus,Jogadores  #Importa os dois modelos criados
 
-admin.site.register(Trofeus)
-admin.site.register(Jogadores)
+admin.site.register(Trofeus) #Regitra o modelo Trofeus para ser utilizado na interface administrativa do Django
+admin.site.register(Jogadores) #Regitra o modelo Jogadores para ser utilizado na interface administrativa do Django
 
 
